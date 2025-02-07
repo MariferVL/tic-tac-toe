0149c4a4c5c4fc0c8a77a3a7c959632f6d01395e
MariferVL <mfvl.1186@gmail.com>
2025-02-07 20:33:46 -0300

    Add simple python code version

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ def check_draw(board):
 
 # Main function to play Tic-Tac-Toe
 def tic_tac_toe():
-    """Main function to play Tic-Tac-Toe."""
-    print("\n🎉 Welcome to Tic-Tac-Toe! 🎉")
+    """Main function to play Tic Tac Toe."""
+    print("\n🎉 Welcome to Tic Tac Toe! 🎉")
     print("\nPlayer ❌ goes first.\n")
 
     board = [[' ' for _ in range(3)] for _ in range(3)]
